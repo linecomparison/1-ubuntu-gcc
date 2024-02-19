b8ee525f7cb08ea26da5d49619e08c6c28b3520c
qiao.guo <tt@tt.com>
2024-02-19 09:14:06 +0000

    init

---
+++ a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "h.h"
+
+void main() {
+    int a = 1;
+    int b = 2;
+    int c = a+b;
+
+    printf("dddd! s=%s\n", s1);
+}
---
+++ h.h
@@ -0,0 +1,5 @@
+char s1[] = "safdasfasf";
+char s2[] = "asdfasdf";
+char s3[] = "fffffff";
+
+void show();
